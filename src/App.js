b0d5cbf85f026964d3968247a7101c205097a5db
Nikhil-hellDev <72862024+Nikhil-hellDev@users.noreply.github.com>
2020-10-14 16:00:31 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import AllRouter from './component/AllRouter'
+
+
+function App() {
+  return (
+    <div>
+       <AllRouter/>
+      
+    </div>
+   
+  );
+}
+
+export default App;
